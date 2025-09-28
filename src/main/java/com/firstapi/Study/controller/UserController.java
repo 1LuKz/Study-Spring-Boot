25dc21694b252a29764207c08cd51b635049d547
1LuKz <lucasedu292003@gmail.com>
2025-09-27 21:53:09 -0300

    Estudando e criando um CRUD simples em Spring Boot

---
+++ src/main/java/com/firstapi/Study/controller/UserController.java
@@ -0,0 +1,64 @@
+package com.firstapi.Study.controller;
+
+import com.firstapi.Study.dto.UserDto;
+import com.firstapi.Study.model.UserModel;
+import com.firstapi.Study.repository.UserRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public ResponseEntity<List<UserModel>> findAll(){
+        List<UserModel> userModelList = userRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(userModelList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity findById(@PathVariable UUID id){
+        Optional<UserModel> user = userRepository.findById(id);
+        if(user.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        return ResponseEntity.status(HttpStatus.FOUND).body(user.get());
+    }
+
+    @PostMapping
+    public ResponseEntity createUser(@RequestBody UserDto userDto){
+        var user = new UserModel();
+        BeanUtils.copyProperties(userDto, user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteUser(@PathVariable UUID id){
+        Optional<UserModel> user = userRepository.findById(id);
+        if(user.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        userRepository.delete(user.get());
+        return ResponseEntity.status(HttpStatus.OK).body("User deleted");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateUser(@PathVariable UUID id, @RequestBody UserDto userDto){
+        Optional<UserModel> user = userRepository.findById(id);
+        if(user.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        var userModel = user.get();
+        BeanUtils.copyProperties(userDto, userModel);
+        return ResponseEntity.status(HttpStatus.OK).body(userRepository.save(userModel));
+    }
+}
---
+++ src/main/java/com/firstapi/Study/model/UserModel.java
@@ -0,0 +1,59 @@
+package com.firstapi.Study.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private String email;
+    private int age;
+
+    public UserModel(){
+    }
+
+    public UserModel(String name, String email, int age){
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
